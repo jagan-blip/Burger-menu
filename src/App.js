2f2d54c435222e80653ba78b95b5dff1a20bab66
jagan-blip <jaganscientistcr6@gmail.com>
2022-02-18 19:30:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [open,setOpen] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div onClick={(e) => {
+        e.preventDefault();
+        setOpen(!open)
+      }} className={`burgerbtn ${open ? "open" : ""}`}>
+        <div className='cross'>
+
+        </div>
+        <div className='crossleft'>
+
+        </div>
+        <div className='burger
+        '>
+           
+
+
+        </div>
+      </div>
+      <div className={`burgerbtn ${open ? "open" : ""}`}>
+        <label className='spn'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      <div className={`burgerbtn ${open ? "open" : ""}` } id="cross">
+        <label className='spn2'>
+          <span></span>
+          <span></span>
+     
+        </label>
+      </div>
     </div>
   );
 }
